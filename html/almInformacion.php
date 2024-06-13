35633db901a593c1fc08edc00a89c4a4c0b3296f
ElMorganazoC4 <137124792+ElMorganazoC4@users.noreply.github.com>
2024-06-12 17:18:27 -0700

    Add files via upload

---
+++ html/almInformacion.php
@@ -0,0 +1,93 @@
+<?php require("sidebar.php"); ?>
+    <section class="home">
+        <div class="title">Almacenamiento de Informacion (Medidas de Almacenamiento)</div>
+        <div align = "justify" class="text"> 
+        Se refieren a las unidades utilizadas para cuantificar la cantidad de datos que pueden ser almacenados en dispositivos de almacenamiento digital, como discos duros, memorias USB, tarjetas de memoria y servidores de almacenamiento en la nube.
+        </div>
+        <div align = "Justify" class="text">
+            <table class="tablaMAI">
+                <tr> <!-- fila 1 -->
+                    <th align="center"><b>Unidad</b></th>
+                    <th align="center"><b>Valor</b></th>
+                    <th align="center"><b>Tamaño (en bytes)</b></th>
+                </tr>
+                <tr> <!-- fila 2 -->
+                    <td align="center">bit (b)</td>
+                    <td align="center">Valor</td>
+                    <td align="center">Tamaño (en bytes)</td>
+                </tr>
+            </table>
+        </div>
+        <!-- Parrafo del Bit -->
+        <div align = "justify" class="text">   
+        <ul>
+        <li><b>Bit:</b>
+            <ul class="ulConSangría">
+                <li>La unidad más pequeña de información, puede ser un 0 o un 1.</li>
+            </ul>
+        </li>
+        </ul>  
+        </div>
+        Parrafo del Byte
+        <div align = "justify" class="text">   
+        <ul>
+        <li><b>Byte (B):</b>
+            <ul class="ulConSangría">
+                <li>Conjunto de 8 bits. Suele representar un solo carácter, como una letra.</li>
+            </ul>
+        </li>
+        </ul>  
+        </div>
+        Parrafo del Kilobyte
+        <div align = "justify" class="text">   
+        <ul>
+        <li><b>Kilobyte (KB):</b>
+            <ul class="ulConSangría">
+                <li>1,024 bytes. Equivale a aproximadamente mil caracteres.</li>
+            </ul>
+        </li>
+        </ul>  
+        </div>
+        Parrafo del Megabyte
+        <div align = "justify" class="text">   
+        <ul>
+        <li><b>Megabyte (MB):</b>
+            <ul class="ulConSangría">
+                <li>1,024 kilobytes. Utilizado para medir el tamaño de archivos comunes como documentos y fotos.</li>
+            </ul>
+        </li>
+        </ul>  
+        </div>
+        Parrafo del Gigabyte
+        <div align = "justify" class="text">   
+        <ul>
+        <li><b>Gigabyte (GB):</b>
+            <ul class="ulConSangría">
+                <li>1,024 megabytes. Comúnmente usado para medir la capacidad de almacenamiento de discos duros y memorias USB.</li>
+            </ul>
+        </li>
+        </ul>  
+        </div>
+        Parrafo del Terabyte
+        <div align = "justify" class="text">   
+        <ul>
+        <li><b>Terabyte (TB):</b>
+            <ul class="ulConSangría">
+                <li>1,024 gigabytes. Utilizado para grandes volúmenes de datos, como en servidores y almacenamiento en la nube.</li>
+            </ul>
+        </li>
+        </ul>  
+        </div>
+        Parrafo del Petabyte
+        <div align = "justify" class="text">   
+        <ul>
+        <li><b>Petabyte (PB):</b>
+            <ul class="ulConSangría">
+                <li>1,024 terabytes. Utilizado para enormes centros de datos y grandes volúmenes de almacenamiento.</li>
+            </ul>
+        </li>
+        </ul>  
+        </div>
+    </section>
+</body>
+</html>
---
+++ html/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include("sidebar.php"); ?>
+    <section class="home">
+        <div class="title">Introducción</div>
+        <div align = "justify" class="text"> 
+        En el mundo digital actual, nos encontramos inmersos en un entorno tecnológico cada vez más complejo y en constante evolución. 
+        La rápida proliferación de nuevas tecnologías y herramientas digitales ha transformado la manera en que vivimos, trabajamos y nos 
+        comunicamos. Sin embargo, esta transformación trae consigo un conjunto de desafíos, especialmente para aquellos que están en proceso 
+        de desarrollar su cultura digital y buscan comprender mejor el vasto paisaje tecnológico que los rodea.</div>
+        <div align = "justify" class="text">   
+        Uno de los aspectos fundamentales que debe ser entendido en este contexto es el licenciamiento de software y hardware. 
+        Estos conceptos, aunque pueden parecer técnicos y difíciles de entender, son cruciales para garantizar un uso adecuado y legal
+        de los recursos tecnológicos. La falta de comprensión en esta área puede llevar a situaciones de riesgo significativas, como el uso 
+        indebido de software no licenciado, que no solo es ilegal, sino que también puede exponer a los usuarios a amenazas de seguridad, 
+        sanciones legales y pérdidas económicas. Además, no conocer las condiciones de licenciamiento puede resultar en una incapacidad para
+        optimizar los recursos tecnológicos disponibles, afectando la eficiencia y el rendimiento tanto a nivel personal como empresarial.
+        </div>
+        <div class="aside-section">
+        Dentro del entorno de esta página web, presentaremos una guía completa y accesible sobre los conceptos clave de los requerimientos
+        y licenciamiento de software y hardware. Nuestro objetivo es proporcionar información clara y concisa que permita a nuestros
+        lectores entender estos conceptos fundamentales, evitar riesgos asociados al uso indebido de tecnología y optimizar los recursos disponibles
+        para alcanzar un mayor nivel de competencia y seguridad en el mundo digital.
+        </div>
+    </section>
+</body>
+</html>
